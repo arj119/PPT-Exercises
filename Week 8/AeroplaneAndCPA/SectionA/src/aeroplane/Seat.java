a45c56cf28642474836e5ca091c4d311bbd4e27b
Arjun Banerjee <arj.banerjee@yahoo.co.uk>
2021-03-03 13:58:13 +0000

    implemented first part

--- Week 8/AeroplaneAndCPA/SectionA/src/aeroplane/Seat.java
+++ Week 8/AeroplaneAndCPA/SectionA/src/aeroplane/Seat.java
@@ -1,7 +1,37 @@
 package aeroplane;
 
-public class Seat {
+import java.util.*;
 
-	// TODO: Section A, Tasks 1 and 3
+public class Seat {
+	private final static Set<Integer> EMERGENCY_EXITS = Set.of(1, 10, 30); 
+	private final static int MAXIMUM_ROW = 50;
+	private final static char MAXIMUM_LETTER = 'F';
+	private final static char MINIMUM_LETTER = 'A';
+	private final int row;
+	private final char letter;
+
+	Seat (int row, char letter){
+		this.row = row;
+		this.letter = letter;
+	}
+
+	@Override
+	public String toString(){
+		return Integer.toString(row) + letter;
+	}
 	
+	boolean isEmergencyExit() {
+		return EMERGENCY_EXITS.contains(row);
+	}
+
+	private boolean hasNext() {
+		return !(this.row == MAXIMUM_ROW && this.letter == MAXIMUM_LETTER);
+	}
+
+	// (cond) ? <then> : <else>
+	Seat next() throws NoSuchElementException {
+		if(!hasNext()) throw new NoSuchElementException(); 
+
+		return new Seat(row + 1, (letter == MAXIMUM_LETTER) ? MINIMUM_LETTER : (char) (letter + 1));
+	}
 }
